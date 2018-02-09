714b634ac64aec7c040d9e171bc16613ae103f4d
Jason Dempsey <asterik@gmail.com>
2018-02-09 08:35:18 -0500

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/jasondemps1/clirescue/cmdutil"
+	"github.com/jasondemps1/clirescue/user"
 )
 
 var (
